54de8f0dd41c9857f198779a39c8a1e474245c95
Noguti <142026294+Noguchi2004@users.noreply.github.com>
2026-01-14 10:05:30 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -129,7 +129,7 @@ export default function App() {
           orgao: formData.orgao,
           dataEmissao: formData.dataEmissao,
           fimVigencia: formData.fimVigencia,
-          antecedenciaDias: Number(formData.antecedenciaDias) || 0,
+          antecedenciaDias: formData.antecedenciaDias,
           statusNovoVenc: formData.statusNovoVenc,
           gestor: formData.gestor,
           responsael: formData.responsavel
